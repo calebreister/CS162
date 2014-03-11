f24ca11ed3b7e9b4db67b0ee338dcc62cea348c9
Caleb Reister <calebreister@gmail.com>
2014-03-11 14:56:18 -0700

    ImageGenerator Complete

--- ImageGenerator/src/ImageGenerator.cc
+++ ImageGenerator/src/ImageGenerator.cc
@@ -5,14 +5,49 @@ int main()
 {
     Color image[WIDTH][HEIGHT];
     ofstream ppm;
+
+    drawGradiant(image);
+    drawCircle(image);
+
+    ppm.open("GeneratedImage.ppm");
+
+    ppm << "P3" << endl << WIDTH << " " << HEIGHT << " " << 255
+         << endl << endl;
+
+    for (int h = 0; h < HEIGHT; h++)
+    {
+        for (int w = 0; w < WIDTH; w++)
+            ppm << image[w][h];
+        ppm << endl;
+    }
+
+    ppm.close();
 }
 
 void drawGradiant(Color img[][HEIGHT])
 {
-
+    int w;
+    int h;
+    for (h = 0; h < HEIGHT; h++)
+    {
+        for (w = 0; w < 75; w++)
+            img[w][h].r = h;
+        for (w = 75; w < 150; w++)
+            img[w][h].g = h;
+        for (w = 150; w < WIDTH; w++)
+            img[w][h].b = h;
+    }
 }
 
 void drawCircle(Color img[][HEIGHT])
 {
-
+    int w;
+    int h;
+    for (h = 125; h < 175; h++)
+        for (w = 88; w < 138; w++)
+        {
+            img[w][h].r = 255;
+            img[w][h].g = 240;
+        }
 }
+
--- ImageGenerator/src/ImageGenerator.hh
+++ ImageGenerator/src/ImageGenerator.hh
@@ -1,13 +1,26 @@
+#include <iostream>
 #include <fstream>
-//Would have used uint8_t under <cstdint> but
-//the compiler could not resolve the type
+#include <iomanip>
+#include <cinttypes>
 
-const int WIDTH = 200;
+const int WIDTH = 225;
 const int HEIGHT = 300;
 
 struct Color
 {
-        unsigned char r, g, b;
+        uint8_t r = 0;
+        uint8_t g = 0;
+        uint8_t b = 0;
+        //the following overloads for both cout and ofstream
+        friend std::ostream& operator<<(std::ostream& out, Color in)
+        {
+            out.fill('0');
+            out.flags(std::ios::internal);
+            out << std::setw(3) << static_cast<int>(in.r) << " "
+                << std::setw(3) << static_cast<int>(in.g) << " "
+                << std::setw(3) << static_cast<int>(in.b) << "    ";
+            return out;
+        }
 };
 
 void drawGradiant(Color img[][HEIGHT]);
